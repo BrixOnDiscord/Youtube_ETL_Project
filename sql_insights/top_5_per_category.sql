5a919c0f203b862e2c7ab2351be97df2d14ca2f6
BrixOnDiscord <85348568+BrixOnDiscord@users.noreply.github.com>
2025-05-17 21:55:46 +0800

    Update top_5_per_category.sql
    Insights #1 extracted on the dataset generate by the ETL pipeline

--- sql_insights/top_5_per_category.sql
+++ sql_insights/top_5_per_category.sql
@@ -1,1 +1,25 @@
+-- Step 1: Created a table
+CREATE TABLE IF NOT EXISTS top_5_most_viewed_youtube_videos_by_category(
+title TEXT NOT NULL,
+categoryid INT NOT NULL, 
+channel_title TEXT NOT NULL, 
+viewcount INT NOT NULL, 
+likecount INT NOT NULL
+);
 
+-- Step 2: Made a CTE on extracting top 5 by the use of window functions
+WITH rank as (
+SELECT ROW_NUMBER() OVER(PARTITION BY categoryid ORDER BY viewcount DESC) AS rn, title, categoryid, channel_title, viewcount, likecount
+FROM youtube_data
+)
+
+  -- Step 3: Inserted the data into the table
+INSERT INTO top_5_most_viewed_youtube_videos_by_category(
+SELECT title, categoryid, channel_title, viewcount, likecount
+FROM rank
+WHERE rn <=5
+);
+
+  -- Read
+SELECT * 
+FROM top_5_most_viewed_youtube_videos_by_category
